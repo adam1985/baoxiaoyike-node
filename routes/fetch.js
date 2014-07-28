77cc4ef9746a19b7b0322a73a1662b5c7a2591c8
pangyuanxing <308106832@qq.com>
2014-07-28 18:15:49 +0800

    微信发布记录

--- routes/fetch.js
+++ routes/fetch.js
@@ -10,30 +10,78 @@ var http = require("http");
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 
+var trim = function(str) {
+    return str.replace(/^\s+|\s+$/g, '');
+};
+
+var randomName = function( nameList , size) {
+    var index = 0, res = [];
+    nameList = nameList.concat();
+    (function(){
+        if( index < size ) {
+            var len = nameList.length, random = Math.floor(Math.random() * len);
+            res.push( trim(nameList.splice( random, 1)[0]) );
+            index++;
+            arguments.callee();
+        }
+    }());
+
+    return res;
+};
+
 exports.fetchresult = function(req, res){
-
-
 
 	dateFormat.format();
 	var initTime = new Date();
-	var dateString = initTime.format("yyyyMMddhhmmss"), dirPath = './create/' + dateString;
+	var dateString = initTime.format("yyyyMMddhhmmss"),
+        dirPath = './create/' + dateString,
+        listPath = dirPath + '/list.txt',
+        jsonPath = dirPath + '/json.txt',
+        usernamePath = './username.txt';
 	fs.mkdirSync(dirPath, {mode : 'r+'});
 	//spawn('mkdir', [dirPath]);
 
+    var createpageMessage = function( data ) {
+        fs.writeFileSync(listPath, '');
+        if( data.length ){
+            data.forEach(function(v){
+                fs.appendFileSync(listPath, v.sourcePage + '\r\n');
+                fs.appendFileSync(listPath, v.title + '\r\n');
+                fs.appendFileSync(listPath, v.username + '\r\n');
+                fs.appendFileSync(listPath, v.viewSource + '\r\n');
+                if(v.pages.length){
+                    v.pages.forEach(function(page){
+                        fs.appendFileSync(listPath, page.imgSrc + '\r\n');
+                        fs.appendFileSync(listPath, page.pageUrl + '\r\n');
+                        fs.appendFileSync(listPath, '\r\n');
+                    });
+                }
+                fs.appendFileSync(listPath, '\r\n\r\n');
+            });
+        }
+    };
+
+  var usernames = fs.readFileSync(usernamePath).toString().split(/\s+/);
 
   var urls = req.body.urls, pageLinks = [];
   if( urls ){
-	pageLinks = JSON.parse( urls ), pageIndex = 0, length = pageLinks.length,
+	pageLinks = JSON.parse( urls );
+  var pageIndex = 0, length = pageLinks.length,
 	totalPage = [];
+    var userList = randomName(usernames, length);
 	(function(){
 		var args = arguments;
 		if( pageIndex < length ){
-			var targetLink = pageLinks[pageIndex];
+			var targetLink = pageLinks[pageIndex],
+                targetUser = userList[pageIndex];
 			
 			nodegrass.get(targetLink, function (data) {
 				var $ = cheerio.load(data);
 				var singlePage = {}, 
 					title = $('title').text();
+                singlePage.sourcePage = targetLink;
+                singlePage.viewSource = 'http://m.baoxiaoyike.cn';
+                singlePage.username = targetUser;
 				singlePage.title = title;
 				singlePage.pages = [];
 				var iframe = $('iframe');
@@ -45,12 +93,9 @@ exports.fetchresult = function(req, res){
 					if( imgRex.test(pageUrl) ){
 						innerPage.imgSrc = 'http://shp.qpic.cn/qqvideo_ori/0/' + RegExp.$1 + '_496_280/0';
 						innerPage.pageUrl = pageUrl;
-						
 					}
 					singlePage.pages.push(innerPage);
-					
-					//console.log(innerPage);
-					
+
 				});
 				
 				totalPage.push(singlePage);
@@ -109,14 +154,14 @@ exports.fetchresult = function(req, res){
 						}
 					}());
 				} else {
+
+                    fs.writeFileSync(jsonPath,JSON.stringify(totalPage) );
+                    createpageMessage(totalPage);
+
 					res.set({'Content-Type':'text/plain'});
-					res.send(JSON.stringify(totalPage));
+					res.send(JSON.stringify({ suceess : true }));
 				}
 			}());
-			
-			//res.set({'Content-Type':'text/plain'});
-			//res.send(JSON.stringify(totalPage));
-
 
 		}
 		
