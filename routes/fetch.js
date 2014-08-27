c85e6ffd88846dc74aa43038cc9e5d00bae7167d
pangyuanxing <308106832@qq.com>
2014-08-27 11:31:51 +0800

    全面获取v.qq.com视频抓取

--- routes/fetch.js
+++ routes/fetch.js
@@ -29,6 +29,95 @@ var randomName = function( nameList , size) {
     return res;
 };
 
+ //这一段是腾讯视频给的把url转成vid
+    function getVidFromUrl(url) {
+      url = url || window.location.toString();
+      //先从url中分析出vid参数，例如×××××.html?vid=××××××
+      var vid = getUrlParam("vid", url),
+        r;
+      if (!vid) { // 使用新规则生成的专辑单视频页面
+        if (r = url.match(/\/\w{15}\/(\w+)\.html/)) {
+          vid = r[1];
+        }
+      }
+      // 单视频播放页
+      if (!vid) {
+        if (r = url.match(/\/page\/\w{1}\/\w{1}\/\w{1}\/(\w+)\.html/)) {
+          vid = r[1];
+        } else if (r = url.match(/\/(page|play)\/+(\w{11})\.html/)) {
+          vid = r[2];
+        }
+      }
+      // 播客专辑播放页
+      if (!vid) {
+        if (r = url.match(/\/boke\/gplay\/\w+_\w+_(\w+)\.html/)) {
+          vid = r[1];
+        }
+      }
+      return encodeURIComponent(vid);
+    }
+    function getUrlParam(p, u) {
+      var u = u || document.location.toString();
+      var pa = p + "=";
+      var f = u.indexOf(pa);
+      if (f != -1) {
+        var f2 = u.indexOf("&", f);
+        var f2p = u.indexOf("?", f);
+        if (f2p != -1 && (f2 == -1 || f2 > f2p))
+          f2 = f2p;
+        f2p = u.indexOf("#", f);
+        if (f2p != -1 && (f2 == -1 || f2 > f2p))
+          f2 = f2p;
+        if (f2 == -1)
+          return u.substring(f + pa.length);
+        else
+          return u.substring(f + pa.length, f2);
+      }
+      return "";
+    }
+
+    //关于腾讯视频 有问题请找popotang
+    function getUrlVid( link, callback){
+      var vid = "", r, flashvars = "", return_url;
+      //with vid in url query str
+      if( r = link.match( new RegExp("(^|&|\\\\?)vid=([^&]*)(&|$|#)") )) {
+        vid = encodeURIComponent(r[2]);
+        callback( vid );
+      }
+      //with cid in url
+      //如果有连续剧的视频。需要jsonp去腾讯视频后台用cid换取一段json，然后取连续剧的第一集开始播放
+      else if( r = link.match( new RegExp("(http://)?v\\.qq\\.com/cover[^/]*/\\w+/([^/]*)\\.html") ) )
+      {
+        var cid = encodeURIComponent(r[2]), 
+		path = 'http://sns.video.qq.com/fcgi-bin/dlib/dataout_ex?auto_id=137&cid=' + cid + '&otype=json'; 
+		
+		nodegrass.get(path, 
+			function ( data ) {
+				data = data.toString();
+				eval(data);
+				try{
+					vid = QZOutputJson['videos'][0]['vid'];
+				}catch(e){
+				}
+				callback( vid );
+				
+		}, 'utf8')
+
+      }
+      //with vid in url
+      //这里用腾讯视频给的逻辑来做
+      else{
+        vid = getVidFromUrl(link);
+        callback( vid );
+
+      }
+    }
+	
+	var trim = function(str) {
+        return str.replace(/^\s+|\s+$/gm, '');
+    };
+
+
 exports.fetchresult = function(req, res){
 
 	dateFormat.format();
@@ -95,7 +184,7 @@ exports.fetchresult = function(req, res){
 	(function(){
 		var args = arguments;
 		if( pageIndex < length ){
-			var targetLink = pageLinks[pageIndex],
+			var targetLink = trim(pageLinks[pageIndex]),
                 //targetUser = userList[pageIndex],
 				targetUser = '點右边关註»',
 				isWeixin = /mp\.weixin\.qq\.com/.test(targetLink);
@@ -157,7 +246,7 @@ exports.fetchresult = function(req, res){
 				
 
 				
-				var innerPage = {};
+				var innerPage = {}, vid = '';
 				
 				
 				
@@ -171,7 +260,7 @@ exports.fetchresult = function(req, res){
 					args.callee();
 				}
 
-				if( vidRex.test(targetLink) ){
+				/*if( vidRex.test(targetLink) ){
 					var vid = vidRex.exec(targetLink)[1];
 					nodegrass.get('http://vv.video.qq.com/getinfo?vids='+ vid + '&otype=json&callback=baoxiaoyike', 
 					function ( data ) {
@@ -182,7 +271,20 @@ exports.fetchresult = function(req, res){
 						args.callee();
 					});
 
-				}
+				}*/
+
+				
+				getUrlVid(targetLink, function($vid){
+					vid = $vid;
+					nodegrass.get('http://vv.video.qq.com/getinfo?vids='+ vid + '&otype=json&callback=baoxiaoyike', 
+					function ( data ) {
+						data = data.toString();
+						eval(data);
+						
+					}, 'utf8').on('error', function(e) {
+						args.callee();
+					});
+				});
 			}
 			
 			
